1a0d92bd1cc0ffdbf8d3f67a05f1434ced494775
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-02-17 22:20:18 +0800

    fixed icon path

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar(props) {
         >
             <img 
                 className="nav--logo_icon"
-                src="/public/reacticon-small.png"
+                src="public/reacticon-small.png"
             />
             <h3 className="nav--logo_text">ReactFacts</h3>
             
